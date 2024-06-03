14a5935621537b0837decd62c68e332d2d62412c
Ben Kehoe <b_man901@hotmail.com>
2024-06-03 17:35:13 +0100

    Testing creating a file

--- Data_Download/DataDownload.py
+++ Data_Download/DataDownload.py
@@ -6,3 +6,10 @@ p = pathlib.Path(AFL)
 
 p.mkdir(parents=True, exist_ok=True)
 
+fn = "test.txt"
+
+result = str([1,2,3])
+
+filepath = p / fn
+with filepath.open("w", encoding ="utf-8") as f:
+    f.write(result)
