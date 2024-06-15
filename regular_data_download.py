eeb58c80960cd43817967a56080dbd118cd685f8
Ben Kehoe <b_man901@hotmail.com>
2024-06-15 19:00:42 +0100

    Regular data download working for AFL

--- regular_data_download.py
+++ regular_data_download.py
@@ -3,58 +3,33 @@
 
 from datetime import datetime, timedelta
 
-import pandas as pd
-
-from Tools.APICaller.api_class import APICall
-from Tools.Sports import sport_class, sports_dicts
+from Tools.Sports import sports_dicts
 from Tools.DataAccess.sports_data_class import SportsData
 
 
 def save_data():
     """
-    Works through the leagues specified in the leagues dictionary and downloads yesterday's games and odds
+    Works through the leagues specified in the leagues dictionary and downloads games and odds from two days ago
     """
     two_days_ago = datetime.now() - timedelta(2)
-    date = "2024-06-08"
-    # date = datetime.strftime(two_days_ago, "%Y-%m-%d")
-    # current_season = 2024  # Here as a test, will need to change to dynamic
+    # date = "2024-06-15" (Test)
+    date = datetime.strftime(two_days_ago, "%Y-%m-%d")
 
     for sport, leagues in sports_dicts.leagues_dict.items():
         sport = SportsData(sport, sports_dicts.versions_dict[sport])
-        # dm = APICall(sport.get_sport(), sport.get_version())
         for i in leagues:
             league_id = i["league_id"]
             name = i["league_name"]
             current_season = i["current_season"]
-            data = sport.get_games(league_id, current_season, date)
-            for result in range(len(data["response"])):
-                if data["response"][result]["status"]["short"] != "FT":
-                    pass
-                else:
-                    game_id = data["response"][result]["game"]["id"]
-                    teams = data["response"][result]["teams"]
-                    home_team_id = teams["home"]["id"]
-                    home_team_name = teams["home"]["name"]
-                    away_team_id = teams["away"]["id"]
-                    away_team_name = teams["away"]["name"]
-                    scores = data["response"][result]["scores"]
-                    if scores["home"]["score"] > scores["away"]["score"]:
-                        result = "home"
+            games = sport.get_games(league_id, current_season, date)
+            for result in range(len(games["response"])):
+                data = sport.get_dataframe_data(
+                    games, result, league_id, name, current_season
+                )
+                data_frame = sport.get_dataframe(data)
+                sport.save_dataframe(data_frame)
 
-                    d = {
-                        "sport": [sport.get_sport()],
-                        "game_id": [game_id],
-                        "league_id": [league_id],
-                        "league_name": [name],
-                        "season": [current_season],
-                        "home_team_id": [home_team_id],
-                        "home_team_name": [home_team_name],
-                        "away_team_id": [away_team_id],
-                        "away_team_name": [away_team_name],
-                        "score": [10],
-                    }
-                # Remember to put league_id, current season & date into df
-                print(game_id)
+        print("Save complete")
 
 
 save_data()
