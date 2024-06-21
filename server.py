5d55efe0bcab2f0765f63e49668ca01bf9e7f8da
Ben Kehoe <b_man901@hotmail.com>
2024-06-21 18:54:53 +0100

    Playing around with Flask

--- server.py
+++ server.py
@@ -6,3 +6,12 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route("/about")
+def about():
+    return "<p>About</p>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
